0b23ae6bd9deedc90045987a3a902c644a777034
danielkean5 <124378867+danielkean5@users.noreply.github.com>
2023-04-11 14:33:19 +0000

    debug

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import math
+
+def SizeOfPizza():
+    PizzaSize = str(input("What size of pizza would you like? Enter Character \"L\" for large or \"X\" for Extra large"))
+    while True:
+        if (PizzaSize == "L"):
+            return "Large"
+        if (PizzaSize == "X"):
+            return "Extra Large"
+        if (PizzaSize != "X") or (PizzaSize != "L"):
+            PizzaSize = str(('Invalid input, choose "L" or "X"'))
+    
+print(SizeOfPizza())
 
