ac7fa30f9c58f0f66454fc1786aec4b7059f031f
danielkean5 <124378867+danielkean5@users.noreply.github.com>
2023-04-13 14:02:59 +0000

    final commit

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def SizeOfPizza():
         elif (PizzaSize != "X") or (PizzaSize != "L"):
             PizzaSize = str(input('Invalid input, choose "L" or "X"\n'))
     
-print(SizeOfPizza())
 
 def NumberOfToppings():
     Toppings = str(input("How many toppings would you like? Enter amount from 0-4.\n"))
@@ -27,5 +26,23 @@ def NumberOfToppings():
             return "4 toppings"
         Toppings = str(input('Invalid input, choose number between 1-4.\n'))
     
-print(NumberOfToppings())
+def main():
+  price = 0
+  size = SizeOfPizza()
+  if size == "Large":
+    price = 6
+  if size == "Extra Large":
+    price = 10
+  num = NumberOfToppings()
+  if num == "1 topping":
+    price = price + 1
+  elif num == "2 toppings":
+    price = price + 1.75
+  elif num == "3 toppings":
+    price = price + 2.5
+  elif num == "4 toppings":
+    price = price + 3.35
     
+  print(str(round(price*1.13,2))+"$")
+
+main()
